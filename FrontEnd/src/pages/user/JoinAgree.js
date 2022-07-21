6cc213151b69af87af43aad6bebeea36e64d3644
mindot7 <codemj11@gmail.com>
2022-07-22 02:14:27 +0900

    Docs: 이용약관 동의 페이지 버튼 추가

--- FrontEnd/src/pages/user/JoinAgree.js
+++ FrontEnd/src/pages/user/JoinAgree.js
@@ -2,56 +2,71 @@ import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const JoinForm = styled.div`
-  background-color: #131317;
-  width: 40vw;
+const Wrapper = styled.div`
+  background-color: black;
+  width: 100vw;
   height: 100vh;
-  display: table;
-  margin-left: auto;
-  margin-right: auto;
+  display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 40px;
 `;
 
-const UserInput = styled.input`
-  position: relative;
-  height: 64px;
-  width: 280px;
-  top: 7px;
-  font-size: 18px;
-  background-color: #b1a5c8;
-  outline: none;
-  border: 1px solid black;
-  margin: 0px;
-  color: white;
-  border-radius: 20px;
+const NeonLoginWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  border-radius: 40px;
+  height: 700px;
+  background-color: #131317;
+  width: 450px;
+  font-size: 30px;
+`;
+
+const AgreeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+margin-top: 30px;
+display: flex;
+justify-content: space-between;
+width: 120px;
 `;
 
 const Button = styled.button`
-  width: 40px;
-  height: 40px;
-  border-radius: 10px;
-  border: 1px solid black;
-  background-color: #fff;
-  margin: 14px;
-  font-size: 4px;
-  color: #535353;
+  font-size: 1rem;
+  font-weight: 200;
+  font-style: italic;
+  color: #fff;
+  padding: 1rem 1.2rem 1.1rem;
+  border: 0.4rem solid #131317;
+  border-radius: 2rem;
+  text-transform: uppercase;
+  background-color: #131317;
   cursor: pointer;
 `;
 
 const JoinAgree = () => {
-  console.log('개인정보동의창');
+
   return (
     <div>
       <Header/>
-      <JoinForm>
-        <UserInput
-        placeholder="동의받을곳!"
-        />
-        <Link to="/"></Link>
-          <Button>회원가입선택 페이지 넘어가게 하기 일단은 메인으로</Button>
-      </JoinForm>
+      <Wrapper>
+        <NeonLoginWrapper>
+          <AgreeWrapper> 이용정보 동의 받을 곳
+          </AgreeWrapper>
+          <ButtonWrapper>
+            <Link to="/join/type">
+              <Button>NEXT</Button>
+            </Link>
+            <Link to="/">
+              <Button>MAIN</Button>
+            </Link>
+          </ButtonWrapper>
+        </NeonLoginWrapper>
+      </Wrapper>
     </div>
   )
 };
