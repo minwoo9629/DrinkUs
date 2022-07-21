9ff27164864b4c5705907618ea40eef4404a8320
mindot7 <codemj11@gmail.com>
2022-07-22 00:20:18 +0900

    Docs: Header.js signUp --> join

--- FrontEnd/src/components/Header.js
+++ FrontEnd/src/components/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
             </li>
 
             <li>
-              <Link to={"/signUp"}>회원가입</Link>
+              <Link to={"/join"}>회원가입</Link>
             </li>
           </>
         )}
