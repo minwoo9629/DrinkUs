98533188accb8be06300e78464ee32f60e2fd028
gksekqls9808 <gksekqls9808@naver.com>
2022-07-22 17:25:46 +0900

    feat: 회원정보수정 기능 및 인기도 수정 기능 구현

--- BackEnd/drinkus-domain/src/main/java/com/ssafy/drinkus/user/domain/UserRepository.java
+++ BackEnd/drinkus-domain/src/main/java/com/ssafy/drinkus/user/domain/UserRepository.java
@@ -1,7 +1,10 @@
 package com.ssafy.drinkus.user.domain;
 
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -16,4 +19,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     //회원번호로 회원 찾기
     Optional<User> findByUserNo(Long userNo);
+
+    //인기도 수정
+    @Modifying
+    @Query("update User set userPopularity = userPopularity + :popularNum where userNo = :userNo")
+    Integer updatePopularity(@Param("userNo") Long userNo, @Param("popularNum") Integer popularNum);
+
 }
