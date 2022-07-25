845a406a64e42cccbc6b4cd76f749ff13f059dc3
Youji-Sung <anc9432@gmail.com>
2022-07-25 14:47:30 +0900

    feat: header background&font color change

--- FrontEnd/src/components/mainpage/Header.js
+++ FrontEnd/src/components/mainpage/Header.js
@@ -1,68 +1,90 @@
+import { useState, useEffect} from 'react'
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import './Header.css'
 
-const DrinkUsHeader = styled.div`
-  position: fixed;
-  display: flex;
-  padding: 40px 4rem;
-  justify-content: center;
-  background-color: transparent;
-  margin: auto;
-  width: calc(100% - 11rem);
-`;
+// const DrinkUsHeader = styled.div`
+//   position: fixed;
+//   display: flex;
+//   padding: 40px 4rem;
+//   justify-content: center;
+//   background-color: ${(props) => props.color};
+//   margin: auto;
+//   width: calc(100% - 11rem);
+// `;
 
 const HeaderMenu = styled.ul`
   display: flex;
   width: calc(${(props) => props.width}% - 11rem);
   justify-content: ${(props) => props.justify};
-  
 `;
 
 const Header = () => {
+  const [ScrollY, setHeaderColor] = useState(0);
+  const [HeaderStatus, setHeaderStatus] = useState(false);
+
+  const handleColor = () => {
+    setHeaderColor(window.pageYOffset);
+    if(ScrollY > 640) {
+      setHeaderStatus(true);
+    } else {
+      setHeaderStatus(false);
+    }
+  }
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleColor)
+    }
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleColor)
+    }
+  })
+
   const user = useSelector((state) => state.user);
   return (
-    <DrinkUsHeader>
+    <div className={HeaderStatus ? "header active" : "header"}>
       <HeaderMenu width={100} justify={"space-between"}>
         <li>
-          <NavLink to="/">DrinkUs</NavLink>
+          <NavLink to="/" className={HeaderStatus ? "a" : "a light"}>DrinkUs</NavLink>
         </li>
         <li>
-          <NavLink to={"/live"}>술Live</NavLink>
+          <NavLink to={"/live"} className={HeaderStatus ? "a" : "a light"}>술Live</NavLink>
         </li>
         <li>
-          <NavLink to={"/community"}>커뮤니티</NavLink>
+          <NavLink to={"/community"} className={HeaderStatus ? "a" : "a light"}>커뮤니티</NavLink>
         </li>
         {user.isLogin ? (
           <>
             <li>
-              <NavLink to={"/users/{user_no}"}>{user.data.nickName}님</NavLink>
+              <NavLink to={"/users/{user_no}"} className={HeaderStatus ? "a" : "a light"}>{user.data.nickName}님</NavLink>
             </li>
             <li>
               {/* api 문서 나오면 주소 일치시켜야 해 */}
-              <NavLink to={"/alarm"}><i class="far fa-heart"></i></NavLink>
+              <NavLink to={"/alarm"} className={HeaderStatus ? "a" : "a light"}><i class="far fa-heart"></i></NavLink>
             </li>
             <li>
               {/* api 문서 나오면 주소 일치시켜야 해 */}
-              <NavLink to={"/friend"}><i class="fa-solid fa-user-group"></i></NavLink>
+              <NavLink to={"/friend"} className={HeaderStatus ? "a" : "a light"}><i class="fa-solid fa-user-group"></i></NavLink>
             </li>
             <li>
-              <NavLink to={"/logout"}>로그아웃</NavLink>
+              <NavLink to={"/logout"} className={HeaderStatus ? "a" : "a light"}>로그아웃</NavLink>
             </li>
           </>
         ) : (
           <>
             <li>
-              <NavLink to={"/login"}>로그인</NavLink>
+              <NavLink to={"/login"} className={HeaderStatus ? "a" : "a light"}>로그인</NavLink>
             </li>
-
             <li>
-              <NavLink to={"/join"}>회원가입</NavLink>
+              <NavLink to={"/join"} className={HeaderStatus ? "a" : "a light"}>회원가입</NavLink>
             </li>
           </>
         )}
       </HeaderMenu>
-    </DrinkUsHeader>
+    </div>
   );
 };
 
