96c2aa2a8ee7625b31eb3792af2a34dcfe6b93d4
minwoo9629 <minwoo9629@gmail.com>
2022-07-27 16:14:15 +0900

    Feat : 소셜 로그인처리를 위한 기능 추가

---
+++ FrontEnd/src/pages/auth/SocialLogin.js
@@ -0,0 +1,17 @@
+import { useDispatch } from "react-redux";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { SuccessAlert } from "../../lib/sweetAlert";
+import { getUserProfile } from "../../store/actions/user";
+
+const SocialLogin = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const jwtToken = searchParams.get("token");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  sessionStorage.setItem("ACCESS_TOKEN", jwtToken);
+  dispatch(getUserProfile());
+  SuccessAlert("로그인되었습니다", navigate);
+  return <></>;
+};
+
+export default SocialLogin;
