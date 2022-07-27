d4e6d50074b5bc27d18da8bfe6f8b4dbd4396892
minwoo9629 <minwoo9629@gmail.com>
2022-07-27 16:12:53 +0900

    fix : 비밀번호 찾기 코드 수정

--- FrontEnd/src/pages/auth/FindPassword.js
+++ FrontEnd/src/pages/auth/FindPassword.js
@@ -1,64 +1,112 @@
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {LoginFormWrapper, LoginForm, InputWrapper, LoginInput, LoginButton} from "./Login"
-import {Wrapper, RoundedWrapper} from "../../components/styled/Wrapper"
-import axios from "axios"
+import {
+  LoginFormWrapper,
+  LoginForm,
+  InputWrapper,
+  LoginInput,
+  LoginButton,
+} from "./Login";
+import { Wrapper, RoundedWrapper } from "../../components/styled/Wrapper";
+import axios from "axios";
+import { BackButton } from "../../components/common/BackButton";
+import { EmptyAlert, FailAlert } from "../../lib/sweetAlert";
+import styled from "styled-components";
+
+import { client } from "../../api/client";
+
+const GoToButton = styled.button`
+  padding: 12px 24px;
+  border-radius: 11px;
+  background-color: #bdcff2;
+  color: white;
+  font-size: 18px;
+  margin-top: 40px;
+  border: 3px solid gray;
+  cursor: pointer;
+`;
 
 const FindPassword = () => {
-  const [state, setState] = useState("");
+  const [state, setState] = useState({
+    userId: "",
+    type: "find",
+  });
   const userIdInput = useRef();
   const navigate = useNavigate();
 
-  const onHandleInput = (e)=>{
-    setState(e.target.value)
+  const onHandleInput = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  const onHandleSubmit = async (event) =>{
+  const onHandleSubmit = async (event) => {
     event.preventDefault();
-    if(state.length === 0){
-      alert("ID 입력해~")
+    if (state.length === 0) {
+      EmptyAlert("아이디를 입력해주세요");
       return;
     }
 
-    const BASE_URL = "http:localhost:8080"
-    // const response = await axios.get(`${BASE_URL}/users/pw`,{userId: state})
-    // const response = await axios.post(`${BASE_URL}/users/pw`,{userId: state})
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users/1").then(response => response)
-    // const response = await axios.get("https://jsonplaceholder.typicode.com/users/1", {userId: state}).then(response => response)
-    if(response.status === 200){
-      alert("이메일 존재해요")
-      let currentTime = new Date();
-      currentTime.setMinutes(currentTime.getMinutes() + 5)
-      document.cookie = `userId=${state}; path=/; expires=${currentTime}`;
-      document.cookie = `userId=iphone@naver.com; path=/; expires=${currentTime}`;
-      document.cookie = `userNickName=꼬부기`;
-      document.cookie = `accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c`;
-      navigate("/result")
-      // 입력한 id 값 쿠키에 저장하고
-      // 결과 페이지
+    // 비밀번호 찾기 Axios 요청 로직
+    const data = {
+      userName: state.userId,
+    };
+
+    const response = await client
+      .post(`/users/pw`, data)
+      .then((response) => response)
+      .catch((error) => error.response);
+
+    if (response.status === 200) {
+      setState({ ...state, type: "result" });
+    } else {
+      FailAlert(response.data.message);
     }
-  }
+  };
 
   return (
     <Wrapper>
-        <RoundedWrapper  width={"450"} height={"700"} mWidth={"300"} mHeight={"460"}>
-          <LoginFormWrapper>
-            <LoginForm onSubmit={onHandleSubmit}>
-              <InputWrapper>
-                <i className="fas fa-envelope"></i>
-                <LoginInput
-                  value={state.userId}
-                  ref={userIdInput}
-                  name="userId"
-                  onChange={onHandleInput}
-                  placeholder="Email ID"
-                />
-              </InputWrapper>
-              <LoginButton type="submit">비밀번호 찾기</LoginButton>
-            </LoginForm>
-          </LoginFormWrapper>
-        </RoundedWrapper>
-      </Wrapper>
+      {state.type === "find" ? (
+        <>
+          <BackButton />
+          <RoundedWrapper
+            width={"450"}
+            height={"700"}
+            mWidth={"300"}
+            mHeight={"460"}
+          >
+            <LoginFormWrapper>
+              <LoginForm onSubmit={onHandleSubmit}>
+                <InputWrapper>
+                  <i className="fas fa-envelope"></i>
+                  <LoginInput
+                    value={state.userId}
+                    ref={userIdInput}
+                    name="userId"
+                    onChange={onHandleInput}
+                    placeholder="Email ID"
+                  />
+                </InputWrapper>
+                <LoginButton type="submit">비밀번호 찾기</LoginButton>
+              </LoginForm>
+            </LoginFormWrapper>
+          </RoundedWrapper>
+        </>
+      ) : (
+        <>
+          <RoundedWrapper
+            width={"840"}
+            height={"400"}
+            mWidth={"300"}
+            mHeight={"200"}
+            flexDirection={"column"}
+          >
+            <h2 style={{ color: "white", marginTop: "20px" }}>
+              {state.userId}로 임시비밀번호가 전송되었습니다.
+            </h2>
+            <GoToButton onClick={() => navigate("/login")}>로그인</GoToButton>
+          </RoundedWrapper>
+        </>
+      )}
+    </Wrapper>
   );
 };
 
