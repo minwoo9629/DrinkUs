cf18627d832f3560553a96dbbdf27600c9aa7fb5
mindot7 <codemj11@gmail.com>
2022-07-29 15:02:37 +0900

    docs: import 주소 변경 및 profile 추가

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,16 +2,15 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
-// import SignUp from "./pages/auth/SignUp";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import FindPassword from "./pages/auth/FindPassword";
 import Result from "./components/Result";
 import FindId from "./pages/auth/FindId";
-import Join from "./pages/user/Join";
+import Join from "./pages/auth/Join";
 import JoinAgree from "./pages/auth/JoinAgree"
 import JoinType from "./pages/auth/JoinType";
-// import Profile from "./pages/user/profile";
+import Profile from "./pages/auth/Profile";
 function App() {
   return (
     <Provider store={store}>
@@ -20,14 +19,13 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/signUp" element={<SignUp />} /> */}
             <Route path="/findPassword" element={<FindPassword />} />
             <Route path="/result" element={<Result />} />
             <Route path="/findId" element={<FindId/>}/>
             <Route path="/join" element={<Join />} />
             <Route path="/join/agree" element={<JoinAgree />} />
             <Route path="/join/type" element={<JoinType />} />
-            {/* <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </BrowserRouter>
