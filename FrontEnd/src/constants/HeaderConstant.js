f98fea825bfd94f634413b3e07819203e9f0138d
minwoo9629 <minwoo9629@gmail.com>
2022-07-29 14:53:52 +0900

    Chore : FE - 로그인 시 상수 값 수정

--- FrontEnd/src/constants/HeaderConstant.js
+++ FrontEnd/src/constants/HeaderConstant.js
@@ -1,16 +1,15 @@
 export const BASIC_MENU = [
-    {link:"/", menuName: "DrinkUs"},
-    {link:"/live", menuName: "술Live"},
-    {link:"/community", menuName: "커뮤니티"}
-]
+  { link: "/", menuName: "DrinkUs" },
+  { link: "/live", menuName: "술Live" },
+  { link: "/community", menuName: "커뮤니티" },
+];
 
 export const LOGINED_MENU = [
-    {link:"/notification", className: "far fa-heart"},
-    {link:"/friends", className: "fas fa-user-friends"},
-    {link:"/logout", menuName: "로그아웃"},
-]
+  { link: "/notification", className: "far fa-heart" },
+  { link: "/friends", className: "fas fa-user-friends" },
+];
 
 export const UNLOGINED_MENU = [
-    {link:"/login", menuName: "로그인"},
-    {link:"/join", menuName: "회원가입"},
-]
+  { link: "/login", menuName: "로그인" },
+  { link: "/join", menuName: "회원가입" },
+];
