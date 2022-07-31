989534b0b72deda5ac8a6424270310183d36979c
mindot7 <codemj11@gmail.com>
2022-07-31 20:43:30 +0900

    Docs: 소셜 로그인 링크 추가

--- FrontEnd/src/pages/auth/JoinType.js
+++ FrontEnd/src/pages/auth/JoinType.js
@@ -1,4 +1,4 @@
-import Header from "../../components/Header";
+import Header from "../../components/layout/Header";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
