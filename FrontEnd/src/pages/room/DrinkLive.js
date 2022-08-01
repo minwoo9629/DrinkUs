bf8861d7d6e3a2f719fc18148f341e9dbba48a30
Youji-Sung <anc9432@gmail.com>
2022-08-01 17:25:20 +0900

    [chore] 술 Live 페이지 구조 작업

--- FrontEnd/src/pages/room/DrinkLive.js
+++ FrontEnd/src/pages/room/DrinkLive.js
@@ -2,7 +2,24 @@ import { GoToButton } from "../../components/common/buttons/GoToButton";
 import { useNavigate } from "react-router-dom";
 import { BaseFlexWrapper } from "../../components/styled/Wrapper"
 import Header from "../../components/layout/Header";
+import { Wrapper } from "../../components/styled/Wrapper";
+import styled from "styled-components";
 
+const Banner = styled.div `
+  height: 270px;
+  width: 1500px;
+  background-color: #EAF1FF;
+`
+
+const RoomBox = styled.div `
+  height: 300px;
+  width: 400px;
+  background-color: #EAF1FF;
+`
+
+const RecommendLetter = styled.p `
+  color: white;
+`
 
 const DrinkLive = () => {
 
@@ -11,13 +28,18 @@ const DrinkLive = () => {
   return (
   <>
     <Header/>
-    <BaseFlexWrapper>
-    술 Live 페이지입니다.
-    <GoToButton onClick={() => navigate("/createroom")} color={"cornflowerblue"}>방 만들기</GoToButton>
-    <GoToButton onClick={() => navigate("/rooms")} color={"#EAF1FF"}>모든 방 보기</GoToButton>
-    </BaseFlexWrapper>
+    <Wrapper>
+      <Banner>배너입니다.</Banner>
+      <BaseFlexWrapper>
+      <GoToButton onClick={() => navigate("/createroom")} color={"cornflowerblue"}>방 만들기</GoToButton>
+      <GoToButton onClick={() => navigate("/rooms")} color={"#EAF1FF"}>모든 방 보기</GoToButton>
+      </BaseFlexWrapper>
+    </Wrapper>
+    <Wrapper>
+      <RecommendLetter>나와 나이가 비슷한 사람들이 있는 방</RecommendLetter>
+      <RoomBox>방 디테일 링크</RoomBox>
+    </Wrapper>
   </>
-  
   );
 };
 
