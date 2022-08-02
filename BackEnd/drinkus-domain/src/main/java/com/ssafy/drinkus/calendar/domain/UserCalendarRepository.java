be2fb85d3b86d137ca701e623355672030e693ac
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-08-03 00:39:49 +0900

    feat: 일정 게시판 조회기능 구현 완료

--- BackEnd/drinkus-domain/src/main/java/com/ssafy/drinkus/calendar/domain/UserCalendarRepository.java
+++ BackEnd/drinkus-domain/src/main/java/com/ssafy/drinkus/calendar/domain/UserCalendarRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.drinkus.calendar.domain;
 import com.ssafy.drinkus.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserCalendarRepository extends JpaRepository<UserCalendar,Long> {
     
     // 회원 일정 번호로 회원일정 삭제
@@ -11,6 +13,10 @@ public interface UserCalendarRepository extends JpaRepository<UserCalendar,Long>
     // 회원번호와 일정번호로 회원일정이 있는지 확인
     Boolean existsByUserAndCalendarBoard(User user, CalendarBoard calendarBoard);
 
+    // 회원번호와 일정번호로 회원일정 조회
+    UserCalendar findByUserAndCalendarBoard(User user, CalendarBoard calendarBoard);
+
     // 해당 일정번호에 참여한 참가자 수가 몇 명인지 조회
     Long countByCalendarBoard(CalendarBoard calendarBoard);
+
 }
