47ea5b2627252768285f544f029048fe62ae5c87
Youji-Sung <anc9432@gmail.com>
2022-08-04 11:11:52 +0900

    [feat] 방 디테일 창 구조

---
+++ FrontEnd/src/pages/room/RoomDetail.js
@@ -0,0 +1,61 @@
+import Header from "../../components/layout/Header";
+import { Wrapper } from "../../components/styled/Wrapper";
+import { client } from "../../utils/client"
+import styled from "styled-components";
+import { useState } from "react";
+import { useEffect } from "react";
+import { GoToButton } from "../../components/common/buttons/GoToButton";
+
+const Box = styled.div`
+  background-color: white;
+`
+
+const onRoomDetail = async () => {
+  const result = await client
+    .get("https://i7b306.p.ssafy.io/api/rooms/1")
+    .then((response)=> response);
+    return result
+}
+
+
+const RoomDetail = () => {
+
+  const [data, setData] = useState({});
+
+  const changeFunc = async () => {
+    const result = await onRoomDetail()
+    setData(result.data);
+    // return data
+  }
+
+  useEffect(()=>{
+    changeFunc();
+  },[])
+
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <Box>
+          <div>
+            {data.roomName} | {data.category.categoryName}
+          </div>
+          <div>
+            {data.placeTheme}
+          </div>
+          <div>
+            {data.user.userName}
+            {data.user.userImg}
+            인기도{data.user.userPopularity}
+          </div>
+          <div>
+            20대
+          </div>          
+        </Box>
+        <GoToButton>참여하기</GoToButton>
+      </Wrapper> 
+    </>
+  );
+};
+
+export default RoomDetail
