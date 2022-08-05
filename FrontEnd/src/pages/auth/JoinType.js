eca33bf1a40db6d9917f11ecc97d86b88a79a35e
mindot7 <codemj11@gmail.com>
2022-08-05 15:51:13 +0900

    Docs: 회원가입 페이지 디자인 수정중

--- FrontEnd/src/pages/auth/JoinType.js
+++ FrontEnd/src/pages/auth/JoinType.js
@@ -1,6 +1,6 @@
-import Header from "../../components/layout/Header";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { BackButton } from "../../components/common/buttons/BackButton";
 
 // 소셜 로그인 종류 보고 api 문서 페이지에서 url 걸어주기
 const Wrapper = styled.div`
@@ -28,10 +28,18 @@ const JoinForm = styled.form`
   flex-direction: column;
 `;
 
+const JoinWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  background-color: #131317;
+  width: 400px;
+`
+
 const Button = styled.button`
   font-size: 1rem;
   font-weight: 200;
-  font-style: italic;
+  font-family: "맑은고딕";
   color: #fff;
   padding: 1rem 1.2rem 1.1rem;
   border: 0.4rem solid #131317;
@@ -48,35 +56,38 @@ const Button = styled.button`
 const JoinType = () => {
   return (
     <div>
-      <Header />
+      <BackButton />
       <Wrapper>
         <NeonLoginWrapper>
           <JoinForm>
             <h2>회원가입 종류 선택</h2>
-            <Link to="/join">
-              <Button>로컬 회원가입</Button>
-            </Link>
-            {/* <Button name="kakao" onClick={socialLogin}>카카오로 시작하기</Button> */}
-            <Button
-              onClick={() =>
-                window.open(
-                  `http://localhost:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/`
-                )
-              }
-            >
-              카카오로 시작하기
-            </Button>
-            {/* <Button name="google" onClick={socialLogin}>구글로 시작하기</Button> */}
-            <Button
-              onClick={() =>
-                window.open(
-                  `http://localhost:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/`
-                )
-              }
-            >
-              구글로 시작하기
-            </Button>
-            {/* <Button name="naver" onClick={socialLogin}>네이버로 시작하기</Button> */}
+            <JoinWrapper>
+              <Link to="/join">
+                <Button>로컬 회원가입</Button>
+              </Link>
+            </JoinWrapper>
+            <JoinWrapper>
+              <Button
+                onClick={() =>
+                  window.open(
+                    `https://i7b306.p.ssafy.io/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/`
+                  )
+                }
+              >
+                카카오로 시작하기
+              </Button>
+            </JoinWrapper>
+            <JoinWrapper>
+              <Button
+                onClick={() =>
+                  window.open(
+                    `https://i7b306.p.ssafy.io/oauth2/authorization/google?redirect_uri=http://localhost:3000/`
+                  )
+                }
+              >
+                구글로 시작하기
+              </Button>
+            </JoinWrapper>
           </JoinForm>
         </NeonLoginWrapper>
       </Wrapper>
