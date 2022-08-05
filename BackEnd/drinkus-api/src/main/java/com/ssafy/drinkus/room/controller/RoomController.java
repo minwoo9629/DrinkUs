affdfcafb344468789497b3245016991aacfc279
gksekqls9808 <gksekqls9808@naver.com>
2022-08-05 11:01:26 +0900

    fix: 어노테이션 삭제

--- BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/room/controller/RoomController.java
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/room/controller/RoomController.java
@@ -38,7 +38,7 @@ public class RoomController {
     //페이징
     @GetMapping
     public ResponseEntity<Page<RoomListResponse>> findBySearchRequest(@LoginUser User user,
-                                                                      @RequestParam @Valid RoomSearchRequest request,
+                                                                      @Valid RoomSearchRequest request,
                                                                       @PageableDefault Pageable pageable){
         Page<RoomListResponse> body = roomService.findBySearchRequest(user, request, pageable);
         return ResponseEntity.ok().body(body);
