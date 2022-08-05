8852cb4c180eb648bca4d083770fffdd1d802280
minwoo9629 <minwoo9629@gmail.com>
2022-08-05 16:46:44 +0900

    Merge : feature/lmw FE로 merge

--- FrontEnd/src/pages/auth/mypage/MySchedule.js
+++ FrontEnd/src/pages/auth/mypage/MySchedule.js
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { getMySchedule } from "../../../api/MyPageAPI";
+import ProfileTitle from "../../../components/auth/ProfileTitle";
+import PageNation from "../../../components/common/buttons/PageNation";
+import ScheduleList from "../../../components/Schedules/ScheduleList";
+
+const ScheduleTap = styled.div`
+  display: flex;
+  padding: 8px 20px;
+  margin: 0px 80px;
+  border-bottom: 1px solid #6f92bf;
+`;
 
 const MySchedule = () => {
-  return <div style={{ padding: "30px" }}>나의 일정 </div>;
+  const [scheduleState, setScheduleState] = useState({
+    content: [],
+    number: 0,
+    numberOfElements: 0,
+    size: 0,
+    totalPages: 0,
+  });
+  const fetchData = async (pageNum) => {
+    const response = await getMySchedule(pageNum);
+    setScheduleState({ ...response.data });
+  };
+
+  const onHandlePageButton = (pageNum) => {
+    fetchData(pageNum);
+  };
+
+  useEffect(() => {
+    fetchData(scheduleState.number);
+  }, []);
+  console.log(scheduleState);
+  return (
+    <div style={{ padding: "30px" }}>
+      <ProfileTitle isEdit={false} />
+      <div style={{ marginTop: "30px" }}>
+        <ScheduleTap>
+          <div style={{ width: "15%" }}>글 번호</div>
+          <div style={{ width: "55%" }}>내용</div>
+          <div style={{ width: "30%" }}>일시</div>
+        </ScheduleTap>
+        <ScheduleList scheduleList={scheduleState.content} />
+        {scheduleState.content.length !== 0 ? (
+          <>
+            <PageNation
+              onClick={onHandlePageButton}
+              number={scheduleState.number + 1}
+              size={scheduleState.size}
+              totalPages={scheduleState.totalPages}
+            />
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MySchedule;
