55db6efba0df34508a464165a46cadf6807e557c
Youji-Sung <anc9432@gmail.com>
2022-08-05 15:40:33 +0900

    [feat] calendar community router 추가

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -14,6 +14,10 @@ import CreateRoom from "./pages/room/CreateRoom";
 import Rooms from "./pages/room/Rooms";
 import JoinAgree from './pages/auth/JoinAgree'
 import JoinType from './pages/auth/JoinType'
+import RoomDetail from './pages/room/RoomDetail'
+import Calendar from "./pages/calendarcommunity/Calendar";
+// import CalendarList from "./pages/calendarcommunity/CalendarList";
+import CreateCalendar from "./pages/calendarcommunity/CreateCalendar";
 function App() {
   return (
     <Provider store={store}>
@@ -32,6 +36,10 @@ function App() {
             <Route path="/live" element={<DrinkLive />} />
             <Route path="/createroom" element={<CreateRoom />} />
             <Route path="/rooms" element={<Rooms />} />
+            <Route path="/rooms/1" element={<RoomDetail />} />
+            <Route path="/calendar" element={<Calendar />} />
+            {/* <Route path="/calendar/list" element={<CalendarList />} /> */}
+            <Route path="/calendar/create" element={<CreateCalendar />} />
           </Routes>
         </div>
       </BrowserRouter>
