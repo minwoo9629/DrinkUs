35f6fd8e141c05d8e377aae1c67ab1900b5bf42d
mingyu <mingyu1218@naver.com>
2022-08-07 17:50:42 +0900

    Report 엔티티 및 패키지 생성

---
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/report/controller/ReportController.java
@@ -0,0 +1,7 @@
+package com.ssafy.drinkus.report.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+}
---
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/report/query/ReportQueryRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.drinkus.report.query;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ReportQueryRepository {
+}
