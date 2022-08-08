a61cbccee02ccd01baa16caf296d349eecf15a02
kkk <rkarud1234@koreatech.ac.kr>
2022-08-08 16:03:11 +0900

    fix: build error

--- BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/security/handler/StompHandler.java
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/security/handler/StompHandler.java
@@ -1,8 +1,5 @@
 package com.ssafy.drinkus.security.handler;
 
-import com.ssafy.drinkus.redis.ChatRoom.ChatRoomRedisRepository;
-import com.ssafy.drinkus.redis.LoginUser.LoginUser;
-import com.ssafy.drinkus.redis.LoginUser.LoginUserRepository;
 import com.ssafy.drinkus.security.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.Message;
@@ -21,8 +18,6 @@ import java.util.Optional;
 public class StompHandler implements ChannelInterceptor {
 
     private final JwtUtil jwtUtil;
-    private final ChatRoomRedisRepository chatRoomRedisRepository;
-    private final LoginUserRepository loginUserRepository;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
@@ -37,8 +32,6 @@ public class StompHandler implements ChannelInterceptor {
             Long userId = jwtUtil.getUserId(extractToken(accessor));
             String userNickName = jwtUtil.getNickName(extractToken(accessor));
 
-            LoginUser loginUser = LoginUser.createLoginUser(userId, userNickName);
-            loginUserRepository.save(loginUser);
         }
         // 채팅방 구독
         else if(StompCommand.SUBSCRIBE == accessor.getCommand()){
@@ -47,23 +40,12 @@ public class StompHandler implements ChannelInterceptor {
             String roomId = getRoomId(Optional.ofNullable((String) message.getHeaders().get("simpDestination")).orElse("InvalidRoomId"));
             String sessionId = (String) message.getHeaders().get("simpSessionId");
 
-            //방안에 유저 추가
-            chatRoomRedisRepository.addChatRoomUser(Long.valueOf(roomId), sessionId, userId);
-            //방세션 추가
-            chatRoomRedisRepository.addSessionRoom(sessionId, Long.valueOf(roomId));
         }
         // 웹소켓 연결 종료
         else if(StompCommand.DISCONNECT == accessor.getCommand()){
             String sessionId = (String) message.getHeaders().get("simpSessionId");
-            Long roomId = chatRoomRedisRepository.getRoomBySession(sessionId);
-            //방안에 유저 삭제
-            chatRoomRedisRepository.deleteChatRoomUser(roomId, sessionId);
-            //세션 삭제
-            chatRoomRedisRepository.deleteSessionRoom(sessionId);
 
             Long userId = jwtUtil.getUserId(extractToken(accessor));
-            Optional<LoginUser> loginUser = loginUserRepository.findById(userId);
-            loginUserRepository.delete(loginUser.get());
         }
         return message;
     }
