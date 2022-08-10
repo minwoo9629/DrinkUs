c5c1efde5ea7a5cd90bccbadc4008a3105ff0ad7
gksekqls9808 <gksekqls9808@naver.com>
2022-08-10 11:18:35 +0900

    chore: 캘린더 css 적용

--- FrontEnd/src/pages/calendarcommunity/Calendar.js
+++ FrontEnd/src/pages/calendarcommunity/Calendar.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { GoToButton } from "../../components/common/buttons/GoToButton";
+import { CalendarButton } from "../../components/common/buttons/CalendarButton";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/layout/Header";
 import { Wrapper } from "../../components/styled/Wrapper";
@@ -17,14 +17,12 @@ const Calendar = () => {
   const [calendar, setCalendar] = useState([]);
   const [first, setFirst] = useState([]);
   const [last, setLast] = useState([]);
-  const calendarTitle = `${curDate.getFullYear()}년 ${
-    curDate.getMonth() + 1
-  }월`;
+  const calendarTitle = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1
+    }월`;
   const fetchData = async () => {
     const response = await client
       .get(
-        `calendar/month?year=${curDate.getFullYear()}&month=${
-          curDate.getMonth() + 1
+        `calendar/month?year=${curDate.getFullYear()}&month=${curDate.getMonth() + 1
         }`,
       )
       .then((response) => response);
@@ -75,6 +73,34 @@ const Calendar = () => {
   };
 
   // 스타일 지정!!!!!
+  const TopMenuWrap = styled.div`
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+  `;
+
+  const NextMonthButton = styled.button`
+    width: 20px;
+    height: 30px;
+    background-color: white;
+    color: #495F7C;
+    text-transform: uppercase;
+    font-size: 25px;
+    font-weight: 800;
+    border: none;
+    margin: auto 10px;
+  `;
+
+  const CalendarTitle = styled.div`
+    font-size: 25px;
+    color: #6F92BF;
+  `;
+  const CalendarWeek = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    margin: 30px auto 0 auto;
+  `;
+
   const DayOfWeek = styled.div`
         background: darkblue;
         color: #fff;
@@ -82,11 +108,24 @@ const Calendar = () => {
         font-size: 15px;
         line-height: 40px;
         height: 50px;
-        width: 100px;
-        margin: 3px;
-        border: 1px darkblue solid;
-        display: inline-block;
+        width: 100%;
+        border: 1px solid #bdcff2;
+
+        :nth-child(even){
+          background-color: #bdcff2;
+        }
+        :nth-child(odd){
+          background-color: #fff;
+          color: #bdcff2;
+        }
 `;
+
+  const DayOfMonth = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    justify-content: center;
+    margin: auto;
+  `;
 
   const OnDay = styled.div`
         background: tomato;
@@ -95,13 +134,25 @@ const Calendar = () => {
         font-size: 15px;
         line-height: 40px;
         height: 100px;
-        width: 100px;
-        margin: 3px;
+        width: 100%;
         border: 1px tomato solid;
         display: inline-block;
         &:hover{
             background-color: red; 
             transition: all 0.4s linear;
+        }
+
+        :nth-child(even){
+          background-color: #bdcff2;
+        }
+        :nth-child(odd){
+          background-color: #fff;
+          color: #bdcff2;
+        }
+
+        :hover{
+          background-color: #11335c;
+          color: #ffeb57;
         }
 `;
 
@@ -112,10 +163,17 @@ const Calendar = () => {
         font-size: 15px;
         line-height: 40px;
         height: 100px;
-        width: 100px;
-        margin: 3px;
+        width: 100%;
         display: inline-block;
         border: 1px lightgrey solid;
+
+        :nth-child(even){
+          background-color: #bdcff2;
+        }
+        :nth-child(odd){
+          background-color: #fff;
+          color: #bdcff2;
+        }
 `;
 
   const InnerWrapper = styled.div`
@@ -126,60 +184,67 @@ const Calendar = () => {
 
   return (
     <>
-    <Header/>
-    <Wrapper color={'#fff'}>
-    <InnerWrapper>
-    <GoToButton onClick={() => navigate("/calendar/create")} color={"cornflowerblue"}>글쓰기</GoToButton>
-    <GoToButton onClick={() => navigate("/daily")} color={"cornflowerblue"}>일간게시판</GoToButton>
-    <div className="App">
-      <div>
-        <button onClick={onHandleDecreaseMonth}>&#60;</button>
-      </div>
+      <Header />
+      <Wrapper color={'#fff'}>
+        <InnerWrapper>
+          <TopMenuWrap>
+            <div>
+              <CalendarButton onClick={() => navigate("/calendar/create")} color={"#bdcff2"} textColor={"#fff"}>월간</CalendarButton>
+              <CalendarButton onClick={() => navigate("/daily")} color={"#ffffff"} textColor={"#bdcff2"}>일간</CalendarButton>
+            </div>
+            <TopMenuWrap>
+              <div>
+                <NextMonthButton onClick={onHandleDecreaseMonth}>&#60;</NextMonthButton>
+              </div>
 
-      <div>{calendarTitle}</div>
-      <div className="calendarBox">
-        <button onClick={onHandleIncreaseMonth}>&#62;</button>
-      </div>
+              <CalendarTitle>{calendarTitle}</CalendarTitle>
+              <div className="calendarBox">
+                <NextMonthButton onClick={onHandleIncreaseMonth}>&#62;</NextMonthButton>
+              </div>
+            </TopMenuWrap>
+            <CalendarButton onClick={() => navigate("/calendar/create")} color={"#bdcff2"} textColor={"#fff"}>글쓰기</CalendarButton>
+          </TopMenuWrap>
+          <div className="App">
+            <div className="wrapper">
+              <CalendarWeek className="grid date_form date_head">
+                {dayOfWeek.map((item, index) => {
+                  return <DayOfWeek key={index}>{item}</DayOfWeek>;
+                })}
+              </CalendarWeek>
 
-      <div className="wrapper">
-        <div className="grid date_form date_head">
-          {dayOfWeek.map((item, index) => {
-            return <DayOfWeek key={index}>{item}</DayOfWeek>;
-          })}
-        </div>
+              <DayOfMonth className="dateSel">
 
-        <div className="dateSel">
-          {first.map((index) => {
-            return <OffDay key={index} background="lightgrey"></OffDay>;
-          })}
-          {calendar.map((item, index) => {
-            if (index > 0) {
-              if (item) {
-                return (
-                  <OnDay
-                    {...item}
-                    key={index}
-                    onClick={() => {
-                      navigate(`/calendar/${curDate.getFullYear()}/${curDate.getMonth() + 1}/${index}`
-                      , {year: curDate.getFullYear(), month: curDate.getMonth() + 1, index: index});
-                    }}
-                  >
-                    {index}
-                  </OnDay>
-                );
-              } else {
-                return <OffDay key={index} background="smoke">{index}</OffDay>;
-              }
-            }
-          })}
-          {last.map((index) => {
-            return <OffDay key={index} background="lightgrey"></OffDay>;
-          })}
-        </div>
-      </div>
-    </div>
-    </InnerWrapper>
-    </Wrapper>
+                {first.map((index) => {
+                  return <OffDay key={index} background="lightgrey"></OffDay>;
+                })}
+                {calendar.map((item, index) => {
+                  if (index > 0) {
+                    if (item) {
+                      return (
+                        <OnDay
+                          {...item}
+                          key={index}
+                          onClick={() => {
+                            navigate(`/calendar/${curDate.getFullYear()}/${curDate.getMonth() + 1}/${index}`
+                              , { year: curDate.getFullYear(), month: curDate.getMonth() + 1, index: index });
+                          }}
+                        >
+                          {index}
+                        </OnDay>
+                      );
+                    } else {
+                      return <OffDay key={index} background="smoke">{index}</OffDay>;
+                    }
+                  }
+                })}
+                {last.map((index) => {
+                  return <OffDay key={index} background="lightgrey"></OffDay>;
+                })}
+              </DayOfMonth>
+            </div>
+          </div>
+        </InnerWrapper>
+      </Wrapper>
     </>
   );
 }
