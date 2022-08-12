9d06fff9f79822a7b277ffe222c9c123c1aece59
rkarud1234 <rkarud1234@koreatech.ac.kr>
2022-08-12 20:02:57 +0900

    test: websocket cors test

--- BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/config/WebSocketConfig.java
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp")
+        registry.addEndpoint("/api/ws-stomp")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
