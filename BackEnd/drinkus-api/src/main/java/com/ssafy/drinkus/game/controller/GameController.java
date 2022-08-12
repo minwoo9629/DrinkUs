1eec975283afb6fb67c8690fe2a1eff6188ea45e
gksekqls9808 <gksekqls9808@naver.com>
2022-08-12 10:58:30 +0900

    fix: gameservice 복원

--- BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/game/controller/GameController.java
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/game/controller/GameController.java
@@ -1,7 +1,9 @@
 package com.ssafy.drinkus.game.controller;
 
+import com.ssafy.drinkus.game.request.GameIdRequest;
 import com.ssafy.drinkus.game.request.TopicRequest;
 import com.ssafy.drinkus.game.response.BombResponse;
+import com.ssafy.drinkus.game.service.GameService;
 import com.ssafy.drinkus.security.util.JwtUtil;
 import com.ssafy.drinkus.user.response.UserMyInfoResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,49 +22,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
     private final JwtUtil jwtUtil;
     private final SimpMessageSendingOperations messagingTemplate;
-
+    private final GameService gameService;
     // 대화 주제 추천 요청 : 대화 주제 요청 받으면(카테고리 아이디)
     // 카테고리아이디에 해당하는 대화주제중 1개를 random으로 보내줌
     // null -> (모두다), id -> (공통, 해당 id주제)
     @MessageMapping("/topic")
     public void findByCategoryId(TopicRequest request){
-        String topic = "";
+        String topic = gameService.findByCategoryId(request.getCategoryId());
         messagingTemplate.convertAndSend("/sub/chat/room/" + request.getRoomId(), topic);
     }
 
     // 폭탄돌리기 : 폭탄돌리기가 시작됐다는 요청 받으면(room_id)
     // random 초랑 시간(초)내에 눌러야하는 클릭횟수를 보내줌
     @MessageMapping("/bomb/start/{room_id}")
-    public void findByRoomId(@PathVariable("room_id") Long roomId){
-        BombResponse bombStartResponse = null;
-        messagingTemplate.convertAndSend("/sub/chat/room/" + roomId, bombStartResponse);
+    public void findByRoomId(GameIdRequest request){
+        BombResponse bombStartResponse = gameService.findByRoomId();
+        messagingTemplate.convertAndSend("/sub/chat/room/" + request.getRoomId(), bombStartResponse);
     }
 
     // 폭탄 돌리기 결과 : 못누른 사람의 id를 받으면(user_id)
     // 해당 user_id의 회원 정보를 모든 사람들에게 뿌려주기
     @MessageMapping("/bomb/result/{room_id}")
     public void findByUserId( @Header(HttpHeaders.AUTHORIZATION) String bearerToken
-                                            , @PathVariable("room_id") Long roomId){
+                                            , GameIdRequest request){
         Long userId = jwtUtil.getUserId(bearerToken.substring(7));
-        UserMyInfoResponse userMyInfoResponse = null;
-        messagingTemplate.convertAndSend("/sub/chat/room/" + roomId, userMyInfoResponse);
+        UserMyInfoResponse userMyInfoResponse = gameService.findByUserId(userId);
+        messagingTemplate.convertAndSend("/sub/chat/room/" + request.getRoomId(), userMyInfoResponse);
     }
 
     // 랜덤 마시기 : 랜덤 마시기가 시작됐다는 요청 받으면(room_id)
     // 방의 사람들 중에 random으로 user_id 정보를 보내기
     @MessageMapping("/random/{room_id}")
-    public void findUserByRoomId(@PathVariable("room_id") Long roomId){
-        UserMyInfoResponse userMyInfoResponse = null;
-        messagingTemplate.convertAndSend("/sub/chat/room/" + roomId, userMyInfoResponse);
-    }
-
-//    @MessageMapping("/test/{id}")
-//    public void test(@PathVariable("id")Long id){
-//        System.out.println("GameController test"+id);
-//        messagingTemplate.convertAndSend("/sub/chat/room/" + id);
-//    }
-    @MessageMapping("/test")
-    public void test(Object id){
-        System.out.println("GameController.test");
+    public void findUserByRoomId(GameIdRequest request){
+        UserMyInfoResponse userMyInfoResponse = gameService.findUserByRoomId();
+        messagingTemplate.convertAndSend("/sub/chat/room/" + request.getRoomId(), userMyInfoResponse);
     }
 }
--- BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/game/service/GameService.java
+++ BackEnd/drinkus-api/src/main/java/com/ssafy/drinkus/game/service/GameService.java
@@ -1,6 +1,11 @@
 package com.ssafy.drinkus.game.service;
 
+import com.ssafy.drinkus.common.NotFoundException;
+import com.ssafy.drinkus.game.query.TopicQueryRepository;
 import com.ssafy.drinkus.game.response.BombResponse;
+import com.ssafy.drinkus.room.domain.Topic;
+import com.ssafy.drinkus.user.domain.User;
+import com.ssafy.drinkus.user.domain.UserRepository;
 import com.ssafy.drinkus.user.response.UserMyInfoResponse;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -8,24 +13,37 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.ssafy.drinkus.util.RandomUtil.makeRandomTopic;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class GameService {
+    private final TopicQueryRepository topicQueryRepository;
+    private final UserRepository userRepository;
+
     public String findByCategoryId(Long categoryId){
-        String topic = "";
-        return topic;
+        List<Topic> findTopicList = topicQueryRepository.findByCategoryId(categoryId);
+        List<String> topicList = findTopicList.stream()
+                .map(Topic::getTopicContent)
+                .collect(Collectors.toList());
+        return makeRandomTopic(topicList);
     }
 
     public BombResponse findByRoomId(){
-        BombResponse bombResponse = null;
-        return bombResponse;
+        int second = -1;
+        int clickCount = second * 2;
+        return new BombResponse(second, clickCount);
     }
 
     public UserMyInfoResponse findByUserId(Long userId){
-        UserMyInfoResponse userMyInfoResponse = null;
-        return userMyInfoResponse;
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(NotFoundException.USER_NOT_FOUND));
+        return UserMyInfoResponse.from(findUser);
     }
 
     public UserMyInfoResponse findUserByRoomId(){
