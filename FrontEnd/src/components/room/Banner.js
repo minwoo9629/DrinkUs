2d74987dfd4ece698733951a119b48b2be884773
Youji-Sung <anc9432@gmail.com>
2022-08-13 14:53:04 +0900

    [chore] banner 디자인 수정

--- FrontEnd/src/components/room/Banner.js
+++ FrontEnd/src/components/room/Banner.js
@@ -6,7 +6,6 @@ import 'slick-carousel/slick/slick-theme.css';
 const Container = styled.div`
   width: 1200px;
   overflow:hidden;
-  border-radius: 30px;
 `;
 
 const StyledSlider = styled(Slider)`
@@ -15,32 +14,17 @@ const StyledSlider = styled(Slider)`
     }
 `;
 
-const Image = styled.img`
-  max-width:1200px;
-  max-height:1200px;
-`;
-
-const ImageContainer = styled.div `
+const ImageContainer = styled.img `
   height: 270px;
   width: 1200px;
-  border-radius: 30px;
   background-color: #EAF1FF;
   margin-bottom: 100px;
 `
-
-// const imgUrl = require('./image/temp.jpg');
 
 const items = [
   { id: 1, url: 'imgUrl' },
   { id: 2, url: 'imgUrl' },
   { id: 3, url: 'imgUrl' },
-  { id: 4, url: 'imgUrl' },
-  { id: 5, url: 'imgUrl' },
-  { id: 6, url: 'imgUrl' },
-  { id: 7, url: 'imgUrl' },
-  { id: 8, url: 'imgUrl' },
-  { id: 9, url: 'imgUrl' },
-  { id: 10, url: 'imgUrl' },
 ];
 
 const Banner = () => {
@@ -49,24 +33,19 @@ const Banner = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    centerMode: true,
+    focusOnSelect: true,
   };
   return (
     <Container>
-      <h2> Single Item</h2>
-      <StyledSlider {...settings}
-      >
-        {items.map(item => {
-          return (
-            <div key={item.id}>
-              <ImageContainer>
-                <Image src={item.url} />
-              </ImageContainer>
-            </div>
-          );
-        })}
+      <StyledSlider {...settings}>
+        {items.map((item) => (
+            <>
+              <ImageContainer 
+              src={item.url}
+              key={item.id}
+              />
+            </>
+        ))}
       </StyledSlider>
     </Container>
   );
