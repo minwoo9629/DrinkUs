89a77bae830b131cd9128f8028f46d01defda2c4
Youji-Sung <anc9432@gmail.com>
2022-08-14 06:17:51 +0900

    [feat] room live, list 페이지 css, background추가, 나이대 등 수정

--- FrontEnd/src/components/room/LiveListItem.js
+++ FrontEnd/src/components/room/LiveListItem.js
@@ -3,12 +3,38 @@ import { TimeGap } from "../../utils/TimeGap";
 import { useNavigate } from "react-router-dom";
 
 const RoomBox = styled.div`
-  display: flex;
+  padding: 10px 20px 20px 10px;
+  flex-direction: row;
   background-color: white;
-  width: 350px;
-  height: 200px;
+  width: 300px;
+  height: 188px;
   border-radius: 30px;
   margin-right: 10px;
+  box-shadow: inset 0px 0px 4px 4px #BDCFF2;
+`
+
+const ImageWrapper = styled.img`
+  margin: 0px 0px 0px 4px;
+  background-color: gray;
+  width: 300px;
+  height: 140px;
+  border-radius: 30px;
+`
+
+const InfoWrapper = styled.div`
+  margin: 4px 8px 4px 16px;
+  justify-content: space-between;
+  display: flex;
+`
+
+const InfoInnerWrapper = styled.div`
+  height: 20px;
+  font-size: 16px;
+`
+
+const TimeBox = styled.div`
+  margin-left: 16px;
+  color: gray;
 `
 
 const LiveListItem = ({
@@ -25,14 +51,27 @@ const LiveListItem = ({
 
   return (
     <>
-    <RoomBox 
-    onClick={() => navigate(`/rooms/${roomId}`)}
-    >
-      <div>{roomId}</div>
-      <div>{roomName}</div>
-      <div>{peopleLimit}</div>
-      <div>{placeTheme}</div>
-      <div>{timeGap}시간 전</div>
+    <RoomBox>
+      {
+        placeTheme === '술집' ?
+        <ImageWrapper src={process.env.PUBLIC_URL + '/assets/RoomBackground/publichouse.jpg'} onClick={() => navigate(`/rooms/${roomId}`)}/> :
+        placeTheme === '펍' ?
+        <ImageWrapper src={process.env.PUBLIC_URL + '/assets/RoomBackground/pub.jpg'} onClick={() => navigate(`/rooms/${roomId}`)}/> :
+        placeTheme === '칵테일바' ?
+        <ImageWrapper src={process.env.PUBLIC_URL + '/assets/RoomBackground/cocktail.jpg'} onClick={() => navigate(`/rooms/${roomId}`)}/> :
+        <ImageWrapper src={process.env.PUBLIC_URL + '/assets/RoomBackground/outside.jpg'} onClick={() => navigate(`/rooms/${roomId}`)}/>
+      }
+      <InfoWrapper>
+        <InfoInnerWrapper>
+          {roomName}
+        </InfoInnerWrapper>
+        <InfoInnerWrapper>
+          {peopleLimit}
+        </InfoInnerWrapper>
+      </InfoWrapper>
+        <TimeBox>
+          {timeGap}시간 전
+        </TimeBox>
     </RoomBox>
     </>
   )
