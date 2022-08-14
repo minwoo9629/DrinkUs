4df622ac4c4133c4b83c7454ee4c7d3ed7d02423
mingyu <mingyu1218@naver.com>
2022-08-15 01:38:15 +0900

    refactor: 안내 문구 텍스트 파일 위치 변경

--- FrontEnd/src/pages/auth/join/JoinAgree.js
+++ FrontEnd/src/pages/auth/join/JoinAgree.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 import {
@@ -15,8 +15,8 @@ const disagreeColor = "white";
 const agreeColor = "#6F92BF";
 
 // 개인정보 수집 이용 안내, 이용 약관 안내 문구
-const personalInformationGuide = "노팅엄은 이번 시즌 폭풍 영입을 하고 있다. 23년 만에 프리미어리그 무대로 승격한 노팅엄은 구단주의 전폭적인 지원 아래 제시 린가드, 니코 윌리엄스 등 굵직한 영입을 이뤘다. 그 중 맨체스터 유나이티드 출신인 딘 헨더슨도 영입되며 주전 골키퍼 자리를 꿰찼다. 이날 경기에도 헨더슨은 노팅엄의 골문을 지켰다. 초반부터 노팅엄은 웨스트햄을 강하게 밀어붙였다. 결국 전반 46분 린가드의 슈팅에 이어 웨스트햄 골문 앞으로 흐른 공을 아워니이가 밀어 넣으며 선제골을 만들었다. 노팅엄이 리드를 지킬 수 있던 건 헨더슨의 역할이 컸다. 후반이 시작되자마자 포르날스가 오른발 중거리 슈팅을 날렸지만 골대를 맞고 나왔다. 이어 헨더슨의 선방이 빛났다. 토마스 소우체크가 뒤이어 골문 앞에서 헤더 슈팅을 날렸지만 헨더슨이 날아오르며 손으로 쳐냈다. 페널티킥을 막아내기도 했다. 후반 18분 웨스트햄에 페널티킥이 선언됐고 웨스트햄의 에이스 데클란 라이스가 키커로 나섰다. 하지만 헨더슨이 정확히 왼쪽으로 가는 슈팅을 읽어내며 막아냈다. 결국 노팅엄은 23년 만에 프리미어리그 승격 후 첫 승리를 따냈다. 노팅엄의 승리의 주역은 결승골을 터뜨린 아워니이였지만, 헨더슨이 없었다면 승리를 장담할 수 없었다. 헨더슨은 훌륭한 반사신경과 수비 리딩 능력 그리고 무엇보다 페널티킥 선방으로 팀의 승리를 견인했다.";
-const termsGuide = "Drinkus는 blah blah ...";
+let personalInformationGuide = "노팅엄은 이번 시즌 폭풍 영입을 하고 있다. 23년 만에 프리미어리그 무대로 승격한 노팅엄은 구단주의 전폭적인 지원 아래 제시 린가드, 니코 윌리엄스 등 굵직한 영입을 이뤘다. 그 중 맨체스터 유나이티드 출신인 딘 헨더슨도 영입되며 주전 골키퍼 자리를 꿰찼다. 이날 경기에도 헨더슨은 노팅엄의 골문을 지켰다. 초반부터 노팅엄은 웨스트햄을 강하게 밀어붙였다. 결국 전반 46분 린가드의 슈팅에 이어 웨스트햄 골문 앞으로 흐른 공을 아워니이가 밀어 넣으며 선제골을 만들었다. 노팅엄이 리드를 지킬 수 있던 건 헨더슨의 역할이 컸다. 후반이 시작되자마자 포르날스가 오른발 중거리 슈팅을 날렸지만 골대를 맞고 나왔다. 이어 헨더슨의 선방이 빛났다. 토마스 소우체크가 뒤이어 골문 앞에서 헤더 슈팅을 날렸지만 헨더슨이 날아오르며 손으로 쳐냈다. 페널티킥을 막아내기도 했다. 후반 18분 웨스트햄에 페널티킥이 선언됐고 웨스트햄의 에이스 데클란 라이스가 키커로 나섰다. 하지만 헨더슨이 정확히 왼쪽으로 가는 슈팅을 읽어내며 막아냈다. 결국 노팅엄은 23년 만에 프리미어리그 승격 후 첫 승리를 따냈다. 노팅엄의 승리의 주역은 결승골을 터뜨린 아워니이였지만, 헨더슨이 없었다면 승리를 장담할 수 없었다. 헨더슨은 훌륭한 반사신경과 수비 리딩 능력 그리고 무엇보다 페널티킥 선방으로 팀의 승리를 견인했다.";
+let termsGuide = "이용 약관 안내 문구";
 
 const NeonSignAnimationTitle = keyframes`
 0%,
@@ -41,27 +41,6 @@ const NeonSignAnimationTitle = keyframes`
   24%,
 `;
 
-const NeonSignAnimation = keyframes`
-0%,
-  19%,
-  21%,
-  23%,
-  25%,
-  54%,
-  56%,
-  100% {
-    text-shadow: -0.2rem -0.2rem 1rem #fff, 0.2rem 0.2rem 1rem #fff,
-      0 0 2rem ${neon_text_color}, 0 0 4rem ${neon_text_color},
-      0 0 6rem ${neon_text_color}, 0 0 8rem ${neon_text_color},
-      0 0 10rem ${neon_text_color};
-  },
-
-  20%,
-  24%,
-`;
-
-// 체크박스 포커스 주기
-
 const NeonSignTitle = styled.h1`
   font-size: 3rem;
   font-weight: 700;
@@ -78,6 +57,7 @@ const NeonSignTitle = styled.h1`
   }
 
 `;
+
 
 const AgreeForm = styled.div`
   display: flex;
@@ -144,7 +124,7 @@ const JoinAgree = () => {
   const [documentDisplay, setDocumentDisplay] = useState([false, false]);
   const [checkNum, setCheckNum] = useState(0);
   const [textColor, setTextColor] = useState([false, false, false]);
-
+  
   const changeHandler = (id) => {
     // 동의한 총 갯수 설정
     if(!textColor[id]) setCheckNum(checkNum + 1);
