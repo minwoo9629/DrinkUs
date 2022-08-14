0f52d4f18e0e9009b3e076e1eb2989440e020e82
minwoo9629 <minwoo9629@gmail.com>
2022-08-14 23:47:20 +0900

    Feat : 모달 닫기 버튼 컴포넌트화

---
+++ FrontEnd/src/components/common/buttons/ModalCloseButton.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyeldModalCloseButton = styled.button`
+  color: #6f92bf;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 8px;
+  border: none;
+  background-color: transparent;
+`;
+const ModalCloseButton = () => {};
