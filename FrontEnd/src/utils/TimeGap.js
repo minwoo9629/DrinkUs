a21f01c83ed0aa5b3f030d7f9b1b8e24e3f75916
Youji-Sung <anc9432@gmail.com>
2022-08-14 20:46:50 +0900

    [fix] 시간차 계산 로직 수정 중

--- FrontEnd/src/utils/TimeGap.js
+++ FrontEnd/src/utils/TimeGap.js
@@ -1,9 +1,30 @@
 import moment from "moment";
 
 export const TimeGap = (createdDate) => {
-  const now = moment(new Date().toLocaleDateString());
-  const then = moment(createdDate)
+  const now = moment(new Date());
+  const then = moment(createdDate);
 
-  const timeGap = moment(moment.duration(now.diff(then))).format('hh')
-  return timeGap
+  const handleTimeGap = moment.duration(now.diff(then)).asHours()
+  if (handleTimeGap < 1) {
+    const timeGap = 1
+    return timeGap
+  } else if ( handleTimeGap < 2) {
+    const timeGap = 2
+    return timeGap
+  } else if ( handleTimeGap < 3) {
+    const timeGap = 3
+    return timeGap
+  } else if ( handleTimeGap < 4) {
+    const timeGap = 4
+    return timeGap
+  } else if ( handleTimeGap < 5) {
+    const timeGap = 5
+    return timeGap
+  } else if ( handleTimeGap < 6) {
+    const timeGap = 6
+    return timeGap
+  } else if ( handleTimeGap >= 6) {
+    const timeGap = 12
+    return timeGap
+  }
 }
